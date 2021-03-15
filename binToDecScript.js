51856affbb55af02aefd17bcb515f1fbd0f25c29
Victor Oliveira <oliveira.vitoco@gmail.com>
2021-03-15 14:59:21 -0300

    Improving converter to convert decimal to binary and decimal to roman

--- binToDecScript.js
+++ binToDecScript.js
@@ -1,33 +1,44 @@
 // selector variables and general variables
-var binarys = document.querySelector('#binary')
+const binarys = document.querySelector('#binary')
 var button = document.querySelector('#button-one')
-var answerScreen = document.querySelector('#answer-screen-one')
-var reversedArray = []
+const answerScreen = document.querySelector('#answer-screen-one')
+const radioButtonDecimalToBinary = document.querySelector('#decimalToBinary')
+const radioButtonBinaryToDecimal = document.querySelector('#binaryToDecimal')
 
 // functions 
 
+function convertDecToBin() {
+  if (radioButtonDecimalToBinary.checked) {
+    let answerScreenValue = parseInt(answerScreen.value)
+    binarys.value = answerScreenValue.toString(2)
+  }
+}
+
 function convertBinToDec() {
-  var reversedArray = []
-  function revert() {
-    Array.from(binarys.value).forEach(function (binary) {
-      reversedArray.unshift(binary)
+  if (radioButtonBinaryToDecimal.checked) {
+    let reversedArray = []
+    function revert() {
+      Array.from(binarys.value).forEach(function (binary) {
+        reversedArray.unshift(binary)
+      })
+    }
+    revert(reversedArray)
+    let sumCount = 0
+
+    reversedArray.forEach(function (item, index) {
+      sumCount += Number(item) * Math.pow(2, index)
     })
+
+    answerScreen.value = sumCount
+    sumCount = 0
   }
 
-  revert(reversedArray)
-  var sumCount = 0
-
-  reversedArray.forEach(function (item, index) {
-    sumCount += Number(item) * Math.pow(2, index)
-  })
-
-  answerScreen.value = sumCount
-  sumCount = 0
 }
 
 // function callers
 
 button.addEventListener('click', convertBinToDec)
+button.addEventListener('click', convertDecToBin)
 
 
 
--- romanToDecScript.js
+++ romanToDecScript.js
@@ -1,39 +1,66 @@
 // selector variables and general variables
-var inputScreen = document.querySelector('#answer-screen-two')
-var inputRoman = document.querySelector('#roman')
+const inputScreen = document.querySelector('#answer-screen-two')
+const inputRoman = document.querySelector('#roman')
 var button = document.querySelector('#button-two')
-var romanObject = {
-  'I': 1,
-  'V': 5,
-  'X': 10,
-  'L': 50,
-  'C': 100,
-  'D': 500,
-  'M': 1000,
-}
-var romanArray = Object.keys(romanObject)
+const radioButtonRomanToDecimal = document.querySelector('#romanToDecimal')
+const radioButtonDecimalToRoman = document.querySelector('#decimalToRoman')
+
 // functions 
-function convertromToDec() {
-  var toUpper = function (array) {
-    return array.toUpperCase();
+function convertDecToRoman() {
+  if (radioButtonDecimalToRoman.checked) {
+    let inputScreenValue = Number(inputScreen.value)
+
+    var romanM = [ "", "M", "MM", "MMM" ]
+    var romanC = [ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM "]
+    var romanX = [ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" ]
+    var romanI = [ "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+          
+    var decimalM = romanM[parseInt(inputScreenValue / 1000)]
+    var decimalC = romanC[parseInt((inputScreenValue % 1000) / 100)]
+    var decimalX =  romanX[parseInt((inputScreenValue % 100) / 10)]
+    var decimalI = romanI[inputScreenValue % 10]     
+    var concatRomans = (decimalM.trim() + decimalC.trim() + decimalX.trim() + decimalI.trim())
+    inputRoman.value = concatRomans
   }
-  var counterForOutPut = 0
-  var arrayFromInputValue = Array.from(inputRoman.value).map(toUpper)
-  arrayFromInputValue.forEach(function (rom, index) {
-    rom = rom.toUpperCase()
-    if (romanArray.includes(rom)) {
-      counterForOutPut += romanObject[rom]
+}
+
+function convertRomToDec() {
+  if (radioButtonRomanToDecimal.checked) {
+    const romanObject = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000,
     }
-    if (rom == 'I' && romanArray.includes(arrayFromInputValue[index + 1]) && arrayFromInputValue[index + 1] != 'I') {
-      counterForOutPut -= 2
+    const romanKeysArray = Object.keys(romanObject)
+
+    var toUpper = function (array) {
+      return array.toUpperCase();
     }
-  })
-  inputScreen.value = counterForOutPut
+
+    var counterForOutPut = 0
+    var arrayFromInputValue = Array.from(inputRoman.value).map(toUpper)
+
+    arrayFromInputValue.forEach(function (rom, index) {
+      rom = rom.toUpperCase()
+      if (romanKeysArray.includes(rom)) {
+        counterForOutPut += romanObject[rom]
+      }
+      if (rom == 'I' && romanKeysArray.includes(arrayFromInputValue[index + 1]) && arrayFromInputValue[index + 1] != 'I') {
+        counterForOutPut -= 2
+      }
+    })
+    inputScreen.value = counterForOutPut
+  }
 }
 
 
 // function callers
-button.addEventListener('click', convertromToDec)
+button.addEventListener('click', convertRomToDec)
+button.addEventListener('click', convertDecToRoman)
 
 
 
